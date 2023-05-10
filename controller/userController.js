abadc9243bf58f5c546ff582b8f6c82860993571
sksabbirhossain <sksabbirhossain4@gmail.com>
2023-05-10 21:19:15 +0600

    user login

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,7 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+//
 const User = require("../model/userSchema");
 
 //create a user
@@ -28,6 +31,54 @@ const createUser = async (req, res, next) => {
   }
 };
 
+//login a user
+const loginUser = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email: email });
+    if (user && user._id) {
+      const isValidPassword = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+      if (isValidPassword) {
+        //prepare the user object to generate token
+        const userObject = {
+          name: user.name,
+          email: user.email,
+          id: user._id,
+        };
+
+        //generate token
+        const token = jwt.sign(userObject, process.env.JWT_SECTET, {
+          expiresIn: 86400000,
+        });
+
+        res.status(200).json({
+          message: "User Login successfull",
+          data: {
+            user: user,
+            accessToken: token,
+          },
+        });
+      } else {
+        res.status(500).json({
+          message: "something worng!",
+        });
+      }
+    } else {
+      res.status(500).json({
+        message: "something went worng!",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
 module.exports = {
   createUser,
+  loginUser,
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 //
-const { createUser } = require("../controller/userController");
+const { createUser, loginUser } = require("../controller/userController");
 
 //CREATE A USER
 router.post("/register", createUser);
+router.post("/login", loginUser);
 
 module.exports = router;
